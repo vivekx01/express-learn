459a1a1959366044d67f02c2a173cbe9403c4d05
Vivek <vivek01.com>
2023-03-25 21:25:40 +0530

    fixed_sec_leak

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose")
 
-// const uri = "mongodb+srv://vivekx01:vivek1234@vivekx01.lf7rj5y.mongodb.net/?retryWrites=true&w=majority"
 const connectDB = (uri)=>{
     console.log("Connecting to Mangu Server");
     return mongoose.connect(uri,{
