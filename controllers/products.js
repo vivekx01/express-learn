349758909b872679a89ba499522e989b83a40b5c
Vivek <vivek01.com>
2023-03-26 17:26:01 +0530

    final_push

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/product")
 
 const getAllProducts = async (req,res) =>{
     //processing user query
-    const {company,name,featured,sort} = req.query
+    const {company,name,featured,sort,select} = req.query
     //creating a queryobject to make api more secure and robust
     const queryobject = {}
     //checking if the user has passed company name
@@ -18,7 +18,7 @@ const getAllProducts = async (req,res) =>{
     if (featured) {
         queryobject.featured= featured;
     }
-    //storing the find query
+    //storing the find query in normal form
     let apiData = Product.find(queryobject)
     //checking if the user has specified any sorting constraint
     if (sort) {
@@ -28,18 +28,29 @@ const getAllProducts = async (req,res) =>{
         //only include sort function in query if the user passes sort param or else normally call query
         apiData = apiData.sort(sortfix)
     }
+    //checking if the user has specified any select constraint (only returns the requested field)
+    if (select) {
+        let selectfix = select.replace(","," ")
+        console.log(selectfix)
+        //only include select function in query if the user passes select param or else normal call
+        apiData= apiData.select(selectfix)
+    }
+    let page = Number(req.query.page) || 1 //fetching number of pages passed and defautl 1
+    let limit = Number(req.query.limit) || 1 //fetching limit passed and default 1
+    let skip = (page-1)*limit //formula for skipping the number of contents (pagination formula)
+    apiData= apiData.skip(skip).limit(limit)
     
     console.log(queryobject)
     //finally performing the query with or without sort param
     const myData = await apiData
-    res.status(200).json({myData})
+    res.status(200).json({myData,nbHits: myData.length})
 }
 
 const getAllProductsTesting = async (req,res) =>{
     console.log(req.query);
     //mention the field for ascending sort & fieldname with minus sign for descending sort (.sort("-price"))
-    const myData = await Product.find(req.query).sort("price") 
-    res.status(200).json({myData})
+    const myData = await Product.find(req.query)
+    res.status(200).json({myData,nbHits: myData.length})
 }
 
 module.exports = {getAllProducts,getAllProductsTesting}
--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const {getAllProducts,getAllProductsTesting} = require("../controllers/products")
 
 router.route("/").get(getAllProducts)
-router.route("/testing").get(getAllProductsTesting)
+router.route("/all").get(getAllProductsTesting)
 
 module.exports = router
